dbf80abd90b8fc52101131bc38720c7636091c7d
Ruslan <mor73353535@yandex.ru>
2020-12-20 21:56:20 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$('.main-slider').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	dots: true,
+	prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow.svg"></button>',
+	nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow.svg"></button>'
+});
+$('.brand-slider').slick({
+	infinite: true,
+	slidesToShow: 7,
+	slidesToScroll: 1,
+});
