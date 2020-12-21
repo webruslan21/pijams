87a046f8f5823ac17eb517f86e9670f3969e62fc
Ruslan <mor73353535@yandex.ru>
2020-12-22 00:31:30 +0300

    index mobile

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,33 @@ $('.main-slider').slick({
 	slidesToScroll: 1,
 	dots: true,
 	prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow.svg"></button>',
-	nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow.svg"></button>'
+	nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow.svg"></button>',
+	responsive: [{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+			}
+		}
+	]
 });
 $('.brand-slider').slick({
 	infinite: true,
 	slidesToShow: 7,
 	slidesToScroll: 1,
+	responsive: [{
+			breakpoint: 992,
+			settings: {
+				arrows: false,
+				slidesToShow: 5,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				slidesToShow: 3
+			}
+		}
+	]
 });
